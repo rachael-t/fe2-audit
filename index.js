d7bc251a79523af1c3899b478f442fa5cf1bdd89
Rachael Thomas <rnthomas001@gmail.com>
2020-03-30 10:07:50 -0600

    Add my working solutions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const audit = {
 
     // What is the value of `this` when we call fn()?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -31,7 +31,7 @@ const audit = {
 
     // What is the value of `this` when we call data.verify())?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'data';
 
     return result;
   },
@@ -40,7 +40,7 @@ const audit = {
 
 
 
-  
+
 
 
   exerciseC() {
@@ -54,8 +54,8 @@ const audit = {
 
     // What is the value of `this` when we call dog.bark();
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
-
+    const result = 'dog';
+    //I see that it is an ES6 arrow function, so the vaue of this is set when the function is created, but I think for this example, the value of this is still the object on both creation and execution (the previous exerciseB)
     return result;
   },
 
@@ -92,9 +92,12 @@ const audit = {
     // Log D: greeting
     greetingGenerator();
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+    const result = [
+      { ‘D’: 'welcome' },
+      { ‘A’: 'hi'},
+      { ‘B’: 'hi' },
+      { ‘C’: 'hi' }
+    ];
 
     return result;
   },
@@ -109,7 +112,7 @@ const audit = {
   exerciseE() {
 
     var shoe = 'heel';
-    // Log A: 
+    // Log A: shoe
 
     function putOnShoe() {
       shoe = 'boot';
@@ -122,9 +125,12 @@ const audit = {
 
     // Log D: shoe
 
-    const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+    const result = [
+      { ‘A’: 'heel' },
+      { ‘C’: 'heel'},
+      { ‘B’: 'boot' },
+      { ‘C’: 'boot' }
+    ];
 
     return result;
   },
@@ -174,7 +180,10 @@ const audit = {
     // e.g.
     // ['Beckon', 'El Five', 'ChoLon', 'Super Mega Bien']
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.reduce((acc, restaurant) => {
+      acc.push(restaurant.name);
+      return acc;
+    }, []);
 
     return result;
   },
@@ -220,7 +229,7 @@ const audit = {
     ];
 
     // Return an array of objects that include just the name of the restaurant and whether takeout is available
-    
+
     // e.g.
     // [
     //    { restaurant: 'Beckon', takeOut: false },
@@ -228,7 +237,13 @@ const audit = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.reduce((acc, restaurant) => {
+      acc.push({
+        `restaurant: ${restaurant.name}`,
+        `takeOut: ${restaurant.takeout}`
+      });
+      return acc;
+    }, []);
 
     return result;
   },
@@ -283,13 +298,16 @@ const audit = {
     // }
 
     const result = 'REPLACE WITH YOUR RESULT HERE';
-
+    //we have an array of objects
+    //we want to create a new object
+    //this object will have three properties: 'New American', 'Tapas/small plates', 'Asian Fusion'
+    //the value of each property will be an array of restaurant names that have a cuisine value that matches one of the three properties -- if it matches, push the restaurant name to the corresponding array
     return result;
   },
 
   exerciseBonus() {
 
-    // There is a war and nobody knows - the alphabet war! 
+    // There is a war and nobody knows - the alphabet war!
     // There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
 
     // Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return 'Left side wins!', when the right side wins return 'Right side wins!', in other case return 'Let's fight again!'.
@@ -329,7 +347,7 @@ const audit = {
       four: alphabetWar('wwwwwwz')
     };
 
-    return result;    
+    return result;
   }
 };
 
